d0cb87af0cd902f13e0426497d5acd671c86e400
Seth1024 is Just another programmer (that likes Ruby) <Seth1024T@users.noreply.github.com>
2017-10-26 13:01:50 -0100

    Update MySecondModule.rb

--- MySecondModule.rb
+++ MySecondModule.rb
@@ -1,4 +1,28 @@
 module MySecondModule
+   
+# This module contains a group of useful fucntions, procedures, and objects. It is considered by many to be a libary
+# of re-usable computer code.
+# This module uses a programming language called Ruby. 
+# keywords: module, function, procedure, object # Copyright (C) 2017 Seth1024T (https://github.com/Seth1024T) 
+# This module was created from bits of code from: https://github.com/Seth1024T/Hello-World 
+# Changes made include the use of the keyword modules, and changing the constant Decoration1 to the function decoration_funct1
+# Because this code is now in a seperate file (that may be expanded to include more code), it may be easily re-used by other
+# programs.
+=begin 
+This program is free software: you can redistribute it and/or modify it under the terms of the 
+GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version. 
+   
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+for more details. You should have received a copy of the GNU General Public License along with this program. 
+If not, see http://www.gnu.org/licenses/. 
+=end 
+# If you want to make the program interactive, then the GPL licence 
+# suggests you include code to let users view the relevant parts of 
+# the GPL licence: http://www.gnu.org/licenses/gpl.html; 
+# see section 17. puts "***************************************" puts "Copyright (C) 2016 Seth1024T (https://github.com/Seth1024T)" puts "This Program uses a GNU GPL V3 licence or alter. Since a " puts "Ruby program must have the source code to run in most cases," puts "please check the source code for details about the GNU GPL V3" puts "licence or go to directly to http://www.gnu.org/licenses/ or" puts "http://www.gnu.org/licenses/gpl.html for more specific details." puts "***************************************"   
+   
 =begin
 in general modules should not include variables, arrays, or instances of objects.
 You can put variables into functions (as is done here) to get around that problem.
