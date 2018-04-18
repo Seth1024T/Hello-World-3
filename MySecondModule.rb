67011c062c8be1b35e6c2b5554967b7704ece3c8
Seth1024 is Just another programmer (that likes Ruby) <Seth1024T@users.noreply.github.com>
2018-04-18 14:51:39 +0000

    Update MySecondModule.rb
    just some minor editing.

--- MySecondModule.rb
+++ MySecondModule.rb
@@ -39,14 +39,15 @@ of useful constants include ones that are used for conversions, or constants tha
 are used in science calculations for example.
 
 Ruby lets programmers do many things, and it is a matter of style on what you chose
-to use. Here modules are for general use (in most cases) and are functions, 
+to use. Here modules are for general use (in most cases) and often contain functions, 
 procedures, and objects.
 
-People that do very low level programming may like to keep variables in arrays in 
-modules, but that level of programming can require specialist knowledge. Knowledge
-not only of low level programming, but quirks about ruby, such as what exactly is 
-an integer representation in ruby--and that might surprise you. However that 
-discussion is outside the scope of this section.
+People that do very low level programming may like to keep variables in arrays that 
+are in turn stored in modules, but that level of programming can require specialist 
+knowledge. Knowledge not only of low level programming, but quirks about ruby, such 
+as what exactly is an integer representation in ruby--and that might surprise you--
+might be required for low level programming. but that discussion is outside the 
+scope of this section, and discussion.
 =end
    
    def LicenceNotice(yr_name_link="2018 Seth1024T (https://github.com/Seth1024T)")
